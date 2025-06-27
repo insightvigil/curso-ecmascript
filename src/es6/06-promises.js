494fb3d52c8c2b1da007a5d00c3d975db507cf12
Adrián Vigil <insightvigil@gmail.com>
2025-06-27 01:29:05 -0600

    promise exercise added

---
+++ src/es6/06-promises.js
@@ -0,0 +1,16 @@
+//Promise
+
+const anotherFunction = () => {
+    return new Promise((resolve,reject) => {
+        if(false) {
+            resolve('Hey!')
+        }
+        else {
+            reject('Oh no!')
+        }
+    }) 
+
+}
+
+//Esta función nos da un response
+anotherFunction().then(respone => { console.log("Finalizado con éxito")}).catch(err => console.log("Falló"))
