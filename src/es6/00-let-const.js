19886b7823aba741184b521ffaac1ea54c8fe853
Adrián Vigil <insightvigil@gmail.com>
2025-06-26 18:54:48 -0600

    Directory with exercises addded

---
+++ src/es6/00-let-const.js
@@ -0,0 +1,32 @@
+/*
+//Classic - We can reassing this type of variables
+var lastName = "Vigil"
+lastName ="Flores"
+console.log(lastName)
+
+
+//New Ones - Here we can reassing too but only with let 
+let fruit = 'Apple'
+fruit = 'Kiwi' 
+
+const animal = 'Dog'
+animal = 'Cat'
+
+*/
+
+
+function nameFruit () {
+    if(true) {
+    var fruit1 = 'Apple'
+    let fruit2 = 'Orange'
+    const fruit3 = 'Banana'
+    }
+
+    console.log(fruit1);
+    console.log(fruit2);
+    console.log(fruit3)
+
+}
+
+nameFruit();
+
---
+++ src/es6/01-arrow-functions.js
@@ -0,0 +1,14 @@
+//Arrow functions 
+
+//Traditional
+function square(num) {
+    return num * num;
+}
+
+//Arrow Function 
+const square = (num) => {
+    return num * num
+}
+
+//Arrow simplified (Only with 1 parameter)
+const square = num => num * num
