78f0bf8e28b9cd2ae300edcb43155669103a4514
Adrián Vigil <insightvigil@gmail.com>
2025-06-26 19:09:17 -0600

    Ejercicio Template Literals realizado

---
+++ src/es6/02-strings.js
@@ -0,0 +1,23 @@
+//Traditional
+let hello = 'Hello'
+let world = 'World'
+
+let epicPhrase = hello + ' ' +world + '!'
+
+console.log(epicPhrase)
+
+//Template Strings
+let epicPhrase2 = `${hello} ${world}!` 
+
+console.log(epicPhrase2)
+
+//Multi-line Strings 
+
+//tradicional
+let lorem = 'Esto es un string \n' + 'esto es otra linea'
+
+console.log(lorem)
+
+let lorem2 = `Esto es una frase épica
+y está es su continuación `
+console.log(lorem2)
