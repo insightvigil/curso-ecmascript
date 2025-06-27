e774bef9254eba306dbf307bd0420d2658a1b743
Adrián Vigil <insightvigil@gmail.com>
2025-06-26 19:39:51 -0600

    Rest-Spread Updated

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -5,6 +5,8 @@ let [fa, fb, fc] = fruits;
 
 console.log(fa,fb,fc)
 
+
+//Object destructuring 
 let user = {
     name: 'Vigil',
     password:  '123'
@@ -12,3 +14,22 @@ let user = {
 
 let {name, password} = user
 console.log(`Hola mi nombre es ${name} y mi pass es ${password}`)
+
+
+//Spread Operator ...
+let user1 = {
+    name: 'Oscar',
+    password:  '2'
+}
+
+let country = 'MX'
+
+let newUser1 = {...user1, country}
+console.log(newUser1)
+
+// rest
+function sum(num,...values) {
+    return console.log(num + values)
+}
+
+sum(10,10,10,10,10)
